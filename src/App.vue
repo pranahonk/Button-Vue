028143bcc8ce995e236d3e765a6e8258649d40f3
yolo001 <pranajobs@hotmail.com>
2020-01-08 23:58:00 +0700

    second commit add more button

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <style>
-.box {
-  width: 120px;
-  text-align: center;
-  display: inline-block;
-}
+  .box {
+    width: 120px;
+    text-align: center;
+    display: inline-block;
+  }
 
-.divider {
-  display: block;
-  margin-top: 15px;
-}
+  .divider {
+    display: block;
+    margin-top: 15px;
+  }
 </style>
 
 
 <template>
   <div>
     <div class="divider">
-      <div v-for="(item, index) in buttonType" :key="index" class="box">
-        <Button :label="item.label" :type="item.type" :size="item.size" :Outline="item.outline"/>
+      <div v-for="(item, index) in buttonType.primary" :key="index" class="box">
+        <Button
+          :label="item.type.charAt(0).toUpperCase() + item.type.slice(1, item.type.length)"
+          :type="item.type"
+          :size="item.size"
+          :Outline="item.outline"
+        />
+        <div>{{item.size}}</div>
+      </div>
+    </div>
+    <div class="divider">
+      <div v-for="(item, index) in buttonType.secondary" :key="index" class="box">
+        <Button
+          :label="item.type.charAt(0).toUpperCase() + item.type.slice(1, item.type.length)"
+          :type="item.type"
+          :size="item.size"
+          :Outline="item.outline"
+        />
         <div>{{item.size}}</div>
       </div>
     </div>
@@ -24,26 +40,61 @@
 </template>
 
 <script>
-import Button from "./components/Button.vue";
+  import Button from "./components/Button.vue";
 
-export default {
-  components: {
-    Button: Button
-  },
-  data() {
-    return {
-      buttonType: [
-        {
-          type: "primary",
-          label: "Primary",
-          outline: false,
-          size: "normal"
-        },
-        { type: "primary", label: "Primary", outline: true, size: "normal" },
-        { type: "primary", label: "Primary", outline: false, size: "large" },
-        { type: "primary", label: "Primary", outline: false, size: "small" }
-      ]
-    };
-  }
-};
+  export default {
+    components: {
+      Button: Button
+    },
+    data() {
+      return {
+        buttonType: {
+          primary: [
+            {
+              type: "primary",
+              outline: false,
+              size: "normal"
+            },
+            {
+              type: "primary",
+              outline: true,
+              size: "normal"
+            },
+            {
+              type: "primary",
+              outline: false,
+              size: "large"
+            },
+            {
+              type: "primary",
+              outline: false,
+              size: "small"
+            }
+          ],
+          secondary: [
+            {
+              type: "secondary",
+              outline: false,
+              size: "normal"
+            },
+            {
+              type: "secondary",
+              outline: true,
+              size: "normal"
+            },
+            {
+              type: "secondary",
+              outline: false,
+              size: "large"
+            },
+            {
+              type: "secondary",
+              outline: false,
+              size: "small"
+            }
+          ]
+        }
+      };
+    }
+  };
 </script>
