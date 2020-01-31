72e7b1e24158627f24e361c89f31044cd17b1cd2
Prana Apsara W <42761991+pranahonk@users.noreply.github.com>
2020-01-31 18:25:04 +0700

    dependencies
    add some dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <style>
-  .box {
-    width: 120px;
-    text-align: center;
-    display: inline-block;
-  }
+.box {
+  width: 120px;
+  text-align: center;
+  display: inline-block;
+}
 
-  .divider {
-    display: block;
-    margin-top: 15px;
-  }
+.divider {
+  display: block;
+  margin-top: 15px;
+}
 </style>
 
 
@@ -36,65 +36,98 @@
         <div>{{item.size}}</div>
       </div>
     </div>
+    <div class="divider">
+      <div v-for="(item, index) in buttonType.success" :key="index" class="box">
+        <Button
+          :label="item.type.charAt(0).toUpperCase() + item.type.slice(1, item.type.length)"
+          :type="item.type"
+          :size="item.size"
+          :Outline="item.outline"
+        />
+        <div>{{item.size}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import Button from "./components/Button.vue";
+import Button from "./components/Button.vue";
 
-  export default {
-    components: {
-      Button: Button
-    },
-    data() {
-      return {
-        buttonType: {
-          primary: [
-            {
-              type: "primary",
-              outline: false,
-              size: "normal"
-            },
-            {
-              type: "primary",
-              outline: true,
-              size: "normal"
-            },
-            {
-              type: "primary",
-              outline: false,
-              size: "large"
-            },
-            {
-              type: "primary",
-              outline: false,
-              size: "small"
-            }
-          ],
-          secondary: [
-            {
-              type: "secondary",
-              outline: false,
-              size: "normal"
-            },
-            {
-              type: "secondary",
-              outline: true,
-              size: "normal"
-            },
-            {
-              type: "secondary",
-              outline: false,
-              size: "large"
-            },
-            {
-              type: "secondary",
-              outline: false,
-              size: "small"
-            }
-          ]
-        }
-      };
-    }
-  };
+export default {
+  components: {
+    Button: Button
+  },
+  data() {
+    return {
+      buttonType: {
+        primary: [
+          {
+            type: "primary",
+            outline: false,
+            size: "normal"
+          },
+          {
+            type: "primary",
+            outline: true,
+            size: "normal"
+          },
+          {
+            type: "primary",
+            outline: false,
+            size: "large"
+          },
+          {
+            type: "primary",
+            outline: false,
+            size: "small"
+          }
+        ],
+        secondary: [
+          {
+            type: "secondary",
+            outline: false,
+            size: "normal"
+          },
+          {
+            type: "secondary",
+            outline: true,
+            size: "normal"
+          },
+          {
+            type: "secondary",
+            outline: false,
+            size: "large"
+          },
+          {
+            type: "secondary",
+            outline: false,
+            size: "small"
+          }
+        ],
+        success: [
+          {
+            type: "success",
+            outline: false,
+            size: "normal"
+          },
+          {
+            type: "success",
+            outline: true,
+            size: "normal"
+          },
+          {
+            type: "success",
+            outline: false,
+            size: "large"
+          },
+          {
+            type: "success",
+            outline: false,
+            size: "small"
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
